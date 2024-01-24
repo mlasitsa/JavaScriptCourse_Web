b57afc1725c9f21ab5f58616dba84322c276532b
Max Lasitsa <150858712+mlasitsa@users.noreply.github.com>
2024-01-23 20:21:25 -0600

    WebPageUpdates

---
+++ WebJsCourse/src/components/IntroBox/IntroBox.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '/src/index.css';
+
+const IntroBox = () => {
+    return (
+        <div className="introBox">
+            <p>INTRODUCTION</p>
+            <p>Installation guide: Visual Studio Code, Node.JS, GIT repository clone</p>
+            <button>COMING SOON</button>
+        </div>
+    )
+}
+
+export default IntroBox; 
